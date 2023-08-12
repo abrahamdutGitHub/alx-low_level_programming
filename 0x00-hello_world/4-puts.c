3dde49c347861b5d93b0470c5e704000135f75db
abrahamdutGitHub <abrahamdut2020@gmail.com>
2023-08-12 05:42:47 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main . a program of comleting language
+'',
+* Return 0 (Success)
+*/
+int main (void)
+{
+    puts ("\" progressing is like building a multilingual puzzle");
+    return (0);
+}
