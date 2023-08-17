077240ac34639a2bca7ac7302ef2d31de8a0ffb4
abrahamdutGitHub <abrahamdut2020@gmail.com>
2023-08-17 06:58:33 -0700

    main0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,14 +1,10 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 
-/**
- * main - Test function for positive or negative
- * Return: 0
- */
-int main(void)
-{
-	int i;
-	i = 0;
-	positive_or_negative(i);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
-	return (0);
-}
+#endif
