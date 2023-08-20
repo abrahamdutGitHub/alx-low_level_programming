d34b1b82818bcb32fe7daab7d4805e07d9c9f870
abrahamdutGitHub <abrahamdut2020@gmail.com>
2023-08-20 03:33:25 -0700

    numbers3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9,
- * followed by a new line
+ * print_numbers - prints 0123456789\n
+ *
+ * Return: void
  */
+
 void print_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = 0; i < 10; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i + '0');
+		_putchar(c);
 	}
-
 	_putchar('\n');
 }
